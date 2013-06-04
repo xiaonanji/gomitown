f7a1e24e5944b4107daa1da1932a1d0516325b40
xiaonanji <sticker.ji@gmail.com>
2013-06-04 22:07:45 +1000

    Update the homepage for the period of development.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to Gomitown</title>
 
 	<style type="text/css">
 
@@ -13,7 +13,7 @@
 	body {
 		background-color: #fff;
 		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		font: 19px/25px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
 
@@ -27,21 +27,10 @@
 		color: #444;
 		background-color: transparent;
 		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
+		font-size: 25px;
 		font-weight: normal;
 		margin: 0 0 14px 0;
 		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
 	}
 
 	#body{
@@ -50,15 +39,13 @@
 	
 	p.footer{
 		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
+		font-style: italic;
 		padding: 0 10px 0 10px;
 		margin: 20px 0 0 0;
 	}
 	
 	#container{
-		margin: 10px;
+		margin: 100px 200px 100px 200px;
 		border: 1px solid #D0D0D0;
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
 	}
@@ -67,21 +54,18 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Gomitown!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Dear visitor,</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>We know you are a gourmet loving beautifully cooked home-made dishes.
+			We know you are an excellent cook delighted when your food is shared and praised by others.
+			We know because we are the same as you.</p>
+		<p>That is why we are working hard to pave the highway leading to Gomitown, 
+		   our best effort to contribute to the community of food lovers.</p>
+		<p class="footer">Gomitowners</p>
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
 </body>
